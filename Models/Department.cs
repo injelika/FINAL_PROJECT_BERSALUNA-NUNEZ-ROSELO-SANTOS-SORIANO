b970c11ff9b4b6ebc03f468a8b7175d941f13499
nielharriss <nielharriss@gmail.com>
2025-05-22 18:39:13 +0800

    Delete Models directory

--- Models/Department.cs
+++
@@ -1,7 +0,0 @@
-public class Department
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public ICollection<User> Users { get; set; } = new List<User>();
-    public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
-}
