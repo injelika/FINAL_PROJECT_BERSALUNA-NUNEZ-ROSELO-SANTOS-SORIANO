b1d566a89a0ae12d4828d51dfcee7e7a87db7464
aethirdsrn <thirdox1204@gmail.com>
2025-05-29 00:59:06 +0800

    Code for RoleAuthorizationMiddleware.cs

---
+++ HelpdeskApp/HelpdeskApp.API/Middleware/RoleAuthorizationMiddleware.cs
@@ -0,0 +1,24 @@
+namespace HelpdeskApp.HelpdeskApp.API.Middleware
+{
+    public class RoleAuthorizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RoleAuthorizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.User.Identity?.IsAuthenticated ?? true)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
